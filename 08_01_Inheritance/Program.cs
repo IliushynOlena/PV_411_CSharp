ba5059d9a1b7991979ca22775f3f9d689903c875
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2025-06-25 20:22:05 +0300

    Inheritance

---
+++ 08_01_Inheritance/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Data;
+
+namespace _08_01_Inheritance
+{
+    abstract class Person: Object
+    {
+        protected string name;
+        protected readonly DateTime birthdate;
+        public Person()
+        {
+            name = "No name";
+            birthdate = new DateTime();
+        }
+        public Person(string name, DateTime b)
+        {
+            this.name = name;
+            if (b < DateTime.Now)
+            {
+                birthdate = b;
+            }
+            else
+                birthdate = new DateTime();
+        }
+        //public void DoWork() = 0;
+        public abstract void DoWork();
+        public virtual void Print()
+        {
+            Console.WriteLine($"Name : {name}.\nBirthdate : {birthdate.ToLongDateString()}");
+        }
+        public override string ToString()
+        {
+            return $"Name : {name}.\nBirthdate : {birthdate}";
+        }
+
+    }
+    //class Name : BaseClass, Interface1, Interface2, Interface3
+    class Worker : Person
+    {
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set 
+            {
+                //if (value < 8000)
+                //    salary = 8000;
+                //else
+                //    salary = value;
+
+                //this.salary = value < 8000 ? 8000 : value;
+                this.salary = value > 8000 ? value : 8000;
+          
+            }
+        }
+        public Worker(): base()
+        {
+            Salary = 0;
+        }
+        public Worker(string name, DateTime b, int salary) : base(name, b)
+        {
+            Salary = salary;
+        }
+        public override void DoWork()
+        {
+            Console.WriteLine("Doing some work !!!");
+        }
+        //new - create a new member and stop virtual
+        //override - continue virtual 
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Salary : {Salary}");
+        }
+    }
+    //sealed - запакований - ми забороняємо перевизначати метод
+    //в наступних наслідниках
+    //sealed - запакований - ми забороняємо наслідування
+    class Programmer : Worker
+    {
+        public int CodeLines { get; set; }
+        public Programmer():base()
+        {
+            CodeLines = 0;  
+        }
+        public Programmer(string name, DateTime b, int salary):base(name,b,salary)
+        {
+            CodeLines = 0;
+        }
+        public void WriteLine()
+        {
+            CodeLines++;
+        }
+        public sealed override void DoWork()
+        {
+            Console.WriteLine("Write code C#!!!");
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"CodeLines : {CodeLines}");
+        }
+    }
+    class TeamLead :Programmer
+    {
+        public int ProjectCount { get; set; }
+        public TeamLead() : base()
+        {
+            ProjectCount = 0;
+        }
+        public TeamLead(string name, DateTime b, int salary) : base(name, b, salary)
+        {
+            ProjectCount = 0;
+        }
+        //public override void DoWork()
+        //{
+        //    Console.WriteLine("Manage team projects!!!");
+        //}
+    }
+   
+ 
+    internal class Program
+    {
+        static void Print()
+        {
+            Console.WriteLine("Heloo");
+        }
+        static void Main(string[] args)
+        {
+            Print();
+
+            Worker worker = new Worker("Vova",new DateTime(2000,12,5), 5000);
+            worker.DoWork();
+            worker.Print();
+
+            Person[] persons = new Person[]
+            {
+                //new Person(),
+                worker,
+                new Programmer("Artem", new DateTime(1995,5,7),6500),
+                new TeamLead("Mukola", new DateTime(1969,6,7), 55000)
+            };
+            
+            foreach (Person person in persons)
+            {
+                Console.WriteLine("------------ Info -----------");
+                person.Print();
+            }
+            Console.WriteLine("-----------------------------------");
+            Programmer? pr = null;
+            int a=0;
+            try
+            {
+                ////------------- 1 use cast()
+                pr = (Programmer)persons[0];
+                pr.DoWork();
+                a = int.Parse(Console.ReadLine());  
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            if( pr != null)
+                 pr.Print();
+            //=====
+            pr?.Print();
+            Console.WriteLine(a);
+            // -------------- 2 use AS-------------
+            pr = persons[1] as Programmer; 
+            if( pr != null )
+            {
+                pr.DoWork();
+            }
+            // -------------- 3 use AS and IS-------------
+            if(persons[0] is Programmer)
+                pr = persons[0] as Programmer;
+           
+
+           
+
+        }
+    }
+}
---
+++ 08_02_Indexers/Program.cs
@@ -0,0 +1,168 @@
+﻿namespace _08_02_Indexers
+{
+    class Laptop
+    {
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public override string ToString()
+        {
+            return $"Model : {Model}. Price : {Price}";
+        }
+    }
+    class Shop
+    {
+        Laptop[] laptops;//null
+        public Shop(int size)//5
+        {
+            laptops = new Laptop[size];
+        }
+        public int Length
+        {
+            get { return laptops.Length; }
+        }
+        public void SetLaptop(int index, Laptop laptop)
+        {
+            if (index >= 0 && index < laptops.Length)
+            {
+                laptops[index] = laptop;
+            }
+        }
+        public Laptop GetLaptop(int index)
+        {
+            if (index >= 0 && index < laptops.Length)
+            {
+                return laptops[index];
+            }
+            throw new IndexOutOfRangeException();
+        }
+        public Laptop this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < laptops.Length)
+                {
+                    return laptops[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < laptops.Length)
+                {
+                    laptops[index] = value;
+                }
+            }
+        }
+        public Laptop this[string name]
+        {
+            //private set
+            //{
+            //    for (int i = 0; i < laptops.Length; i++)
+            //    {
+            //        if (laptops[i].Model == name)
+            //        {
+            //            laptops[i] = value;
+            //        }
+            //    }
+            //}
+            get
+            {
+                foreach (var l in laptops)
+                {
+                    if (l.Model == name)
+                        return l;
+                }
+                return null;
+            }
+        }
+        private int FindByPrice(double price)
+        {
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                if(laptops[i].Price == price)
+                    return i;
+            }
+            return -1;
+        }
+        public Laptop this[double price]
+        {
+            get 
+            { 
+                int index = FindByPrice(price); 
+                if(index != -1)
+                    return laptops[index];
+                throw new Exception("Incorrect price ");
+            }
+            set {
+                int index = FindByPrice(price);
+                if (index != -1)
+                    this[index] = value;
+            }
+        }
+        }
+    public class MultArray
+    {
+        private int[,] array;
+        public int Rows { get; private set; }
+        public int Cols { get; private set; }
+        public MultArray(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            array = new int[rows, cols];
+        }
+        public int this[int r, int c]
+        {
+            get { return array[r, c]; }
+            set { array[r, c] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MultArray multArray = new MultArray(2, 3);
+
+            for (int i = 0; i < multArray.Rows; i++)
+            {
+                for (int j = 0; j < multArray.Cols; j++)
+                {
+                    multArray[i, j] = i + j;//indexator - set
+                    Console.Write($"{multArray[i, j]} ");//indexator - get
+                }
+                Console.WriteLine();
+            }
+            Shop shop = new Shop(3);
+            //shop.SetLaptop(0, new Laptop() { Model = "HP", Price = 25000 });
+            //Console.WriteLine(shop.GetLaptop(0));
+            shop[0] = new Laptop() { Model = "HP", Price = 22333.99 };
+            shop[1] = new Laptop() { Model = "ASUS", Price = 32000.99 };//set
+            shop[2] = new Laptop() { Model = "DELL", Price = 12000.99 };//set
+
+            Console.WriteLine(shop[0]);
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(shop[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(shop["HP"]);
+            //shop["HP"] = new Laptop() {  Model = "Mac", Price = 120000};
+           // Console.WriteLine(shop["Mac"]);
+
+            Console.WriteLine(shop[22333.99]);
+            shop[22333.99] = new Laptop();
+            for (int i = 0; i < shop.Length; i++)
+            {
+                Console.WriteLine(shop[i]);
+            }
+
+        }
+    }
+}
